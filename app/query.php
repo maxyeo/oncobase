e832c6ad06c43fe2ce19047e829513b45f08409e
Max Yeo <mzwyeo@gmail.com>
2015-12-20 17:31:37 -0500

    added, implemented moving graph over years

--- app/query.php
+++ app/query.php
@@ -301,7 +301,6 @@ if (isset($_POST['submit'])) {
 	</div>
 
     <script type="text/javascript" src="js/jquery.js"></script>
-    <script src="//d3js.org/d3.v3.min.js" charset="utf-8"></script>
     <script type="text/javascript" src="js/style.js"></script>
 </body>
 </html>
---
+++ app/years.php
@@ -0,0 +1,47 @@
+<?php
+require 'config.php';
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
+}
+catch(PDOException $e) {
+    exit('Bad Request');
+}
+if (isset($_GET)) {
+	$data_missing = array();
+	if (empty($_GET['race'])) {
+		$data_missing[] = 'race';
+	} else {
+		$race = trim($_GET['race']);
+	}
+	if (empty($_GET['gender'])) {
+		$data_missing[] = 'gender';
+	} else {
+		$gender = trim($_GET['gender']);
+	}
+	if (empty($_GET['cancer'])) {
+		$data_missing[] = 'cancer';
+	} else {
+		$cancer = trim($_GET['cancer']);
+	}
+	if (empty($data_missing)) {
+		$sql = 'SELECT Year, Rate
+				FROM akpsi.onco_cancer_site_breakdown
+				WHERE RaceEthnicity = ? AND Sex = ? AND CancerSite = ?
+		';
+		$stmt = $conn->prepare($sql);
+		try {
+			$stmt->execute(array($race, $gender, $cancer));
+			$rows = array();
+			while ($row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+				$rows[] = $row;
+		    }
+		    echo json_encode($rows);
+		    $stmt = null;
+		} catch(PDOException $e) {
+			exit('Bad Request');
+		}
+	} else {
+		exit('Bad Request');
+	}
+}
+?>
